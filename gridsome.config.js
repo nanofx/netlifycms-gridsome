02fbf157b78b88a05cc553f8c8d3e7d0094bdd9b
nanofx <nanofx01@gmail.com>
2020-08-26 15:12:32 +0300

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,11 +1,11 @@
-o// This is where project configuration and plugin options are located.
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Technologijos',
+  siteName: 'Technologijų Naujienos',
   siteDescription: 'news - gadgets - lifestyle',
 
   templates: {
